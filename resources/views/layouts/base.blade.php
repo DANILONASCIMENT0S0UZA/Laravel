1abd6aea45ec99436ea212e7d09cfdfd46b1cbdb
DANILO NASCIMENTO SOUZA <danilo.nsouza1@senacsp.edu.br>
2023-05-30 16:39:40 -0300

    Laravel

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -8,9 +8,10 @@
     {{-- CSS --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     {{-- JS --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 </head>
@@ -26,5 +27,9 @@
         <p>Quidem, nobis odio reiciendis distinctio nihil totam veniam quibusdam rerum, obcaecati minus ipsam corrupti necessitatibus doloremque quia eligendi similique dolor aut et fugit quis. Omnis earum reprehenderit molestias ex odit.</p>
 
     </div>
+
+    <footer>
+        <p class="fs-5 text-center">T94</p>
+    </footer>
 </body>
 </html>
---
+++ resources/views/usuario/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.base')
+
+@section('content')
+
+<div class="container">
+
+    <h1>Usuários</h1>
+
+    <table class="table table-border table-striped">
+        <thead>
+            <tr>
+                <th>Ações</th>
+                <th>Nome</th>
+                <th>E-mail</th>
+                <th>Criado em </th>
+            </tr>
+        </thead>
+        <tbody>
+
+            <tr>
+                <th></th>
+                <th></th> 
+                <th></th>
+                <th></th>
+            </tr>
+
+        </tbody>
+    </table>
+
+</div>
+
+
